20d647676026f29f75fb27119fac0e6cf4f1ec0d
Manneta Othman <othman.manneta@gmail.com>
2023-08-02 09:32:55 +0100

    full project

---
+++ script.js
@@ -0,0 +1,114 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.querySelector('#nav-menu'),
+      navToggle = document.querySelector('#nav-toggle'),
+      navClose = document.querySelector('#nav-close');
+
+      /* Show Menu */
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    })
+}
+    /* Hidden Menu */
+    if(navClose){
+        navClose.addEventListener('click', () => {
+            navMenu.classList.remove('show-menu');
+        })
+    }
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLinks = document.querySelectorAll('.nav-link');
+
+const linkAction = () => {
+    navMenu.classList.remove('show-menu');
+}
+
+navLinks.forEach(link => link.addEventListener('click', linkAction))
+
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+    const header = document.querySelector('#header');
+
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header');
+}
+
+window.addEventListener('scroll', scrollHeader);
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up');
+
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+    const scrollY = window.pageYOffset;
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop -58,
+              sectionId = current.getAttribute('id'),
+              sectionClass = document.querySelector('.nav-menu a[href*= ' + sectionId +']');
+
+              if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+                sectionClass.classList.add('active-link');
+              }else{
+                sectionClass.classList.remove('active-link');
+              }
+    })
+}
+
+window.addEventListener('scroll', scrollActive);
+
+/*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'ri-sun-line';
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+// We obtain the current theme that the interface has by validating the dark theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
+
+// We validate if the user previously chose a topic
+if(selectedTheme) {
+    // if the validation is fullfilled, we ask what the issue was to know if we activate or desactivate the dark mode
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
+}
+
+// Activate and disactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    //We save the theme and current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+
+})
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+/*     reset: true,
+ */})
+
+sr.reveal(`.home-img, .newsletter-container, .footer-logo, .footer-description, .footer-content, .footer-info`);
+sr.reveal(`.home-data`, {origin: 'bottom'});
+sr.reveal(`.about-data, .recently-data`, {origin: 'left'});
+sr.reveal(`.about-img, .recently-img`, {origin: 'right'});
+sr.reveal(`.popular-card`, {interval: '100'});
